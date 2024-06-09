31252b09649d029bb106633ba011fd6d32e1ef8b
GINTOAHC <chaotingchen10@gmail.com>
2024-06-10 02:11:54 +0800

    style

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -21,10 +21,10 @@ func New(from string, pass string) *Mail {
 func (m *Mail) Send(to, subject, content string) error {
 	// Authentication
 	auth := smtp.PlainAuth("", m.from, m.pass, m.host)
-    // Build mail
-    var body bytes.Buffer
+	// Build mail
+	var body bytes.Buffer
 	mime := "\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-    body.Write([]byte(fmt.Sprintf("Subject: %s \n %s \n\n %s \n", subject, mime, content)))
+	body.Write([]byte(fmt.Sprintf("Subject: %s \n %s \n\n %s \n", subject, mime, content)))
 
 	err := smtp.SendMail(m.host+":"+m.port, auth, m.from, []string{to}, body.Bytes())
 	if err != nil {
