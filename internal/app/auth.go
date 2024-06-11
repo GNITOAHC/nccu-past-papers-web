e49230a4048891eb48d29c32338d0e7ed53ceac6
GINTOAHC <chaotingchen10@gmail.com>
2024-06-11 23:17:19 +0800

    fix: Prompt message

--- internal/app/auth.go
+++ internal/app/auth.go
@@ -23,7 +23,7 @@ func (a *App) Register(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to register user", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte("Success, please wait for approval")) // Write to response first
+	w.Write([]byte("Success, please check your email and wait for approval.")) // Write to response first
 	t, err := template.ParseFiles("templates/mail/register.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
