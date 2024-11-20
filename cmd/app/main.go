7dfd1da81fdec1ece8629e9a32f2397073a6ecbe
BeatriceL <ys20021102@gmail.com>
2024-11-20 20:47:41 +0800

    feat: Search file tree

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"past-papers-web/internal/app"
+	"past-papers-web/internal/config"
+	"past-papers-web/internal/helper"
 )
 
 func main() {
 	app.StartServer()
+
+	// 從配置文件加載設置
+	cfg := config.NewConfig(".env") // 加載環境變量配置
+
+	// 初始化 GitHub Tree 數據
+	treeData := helper.InitTree(cfg)   // 初始化樹數據
+	root := helper.ParseTree(treeData) // 解析樹結構
+
+	// 註冊搜尋處理器
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		helper.SearchHandler(w, r, root)
+	})
+
+	// 啟動 HTTP 服務
+	log.Println("Server is running on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- internal/helper/tree_test.go
+++ internal/helper/tree_test.go
@@ -53,6 +53,37 @@ func TestGetChildren(t *testing.T) {
 	printTree(node, 0)
 }
 
+func TestBFSSearch(t *testing.T) {
+	jsonFile, err := os.Open("./tree_test.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer jsonFile.Close()
+	body, err := io.ReadAll(jsonFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var data map[string]interface{}
+	if err := json.Unmarshal(body, &data); err != nil {
+		t.Fatal(err)
+	}
+
+	root := ParseTree(data)
+
+	// 測試搜尋包含 "alg" 的檔案和資料夾
+	substring := "hw1"
+	results := root.BFSSearch(substring)
+
+	if len(results) == 0 {
+		t.Log("未找到包含子字串:", substring)
+	} else {
+		for _, node := range results {
+			t.Logf("找到: %s at path: %s\n", node.Name, node.Path)
+		}
+	}
+}
+
 func printTree(t *TreeNode, depth int) {
 	isDir := t.IsDir
 	if isDir {
